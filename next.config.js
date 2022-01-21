46dbdbf62802f51e9a03369cd4fa4444fb5dc689
nicknguyen-cs <nick.nguyen@contentstack.com>
2022-01-21 08:52:49 -0800

    .env

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// filename should be next.config.js
+module.exports = {
+    env: {
+        api_key: "blte46a8e4eb8f8d2b4",
+        delivery_token: "cs68bed98922222c41af12bd31",
+        environment: "production",
+        management_token: "cs8b6fc183a534cb679695fad8",
+        content_type_uid: "test",
+        entry_uid: "bltca5562f708655aa1"
+       }
+  }
+  
